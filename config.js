7f2fdce6017ff0e51b4ae03392013a1ac4032eb8
Michał Matysiak <michalmatysiak02@gmail.com>
2018-05-21 09:26:41 +0200

    Add all files

---
+++ config.js
@@ -0,0 +1,5 @@
+module.export = {
+	'GOOGLE_CLIENT_ID': '358226008254-rk4kqudposl5rn00fbmo6s3bj1144qfl.apps.googleusercontent.com',
+	'GOOGLE_CLIENT_SECRET': 'ZT42x71tQFkzO-9OlnmqKROA',
+	'CALLBACK URL': 'http://localhost:3000/auth/google/callback'
+};
