80d6842ad98aa39f24d1e064d23dd1b387e82d86
michalmatysiak <37140076+michalmatysiak@users.noreply.github.com>
2018-05-25 10:04:47 +0200

    Update server.js
    delete console.log

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ var express = require('express');
 var passport = require('passport');
 var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 var config = require('./config');
-console.log("config",config);
 var app = express();
 var googleProfile = {};
 
